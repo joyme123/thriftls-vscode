87e746837b695487bdabdf86bd12cc7412ef93c5
江鹏飞 <jiangpengfei.jiangpf@bytedance.com>
2023-08-14 17:15:25 +0800

    fix: fix path on windows

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,13 +4,14 @@
  * ------------------------------------------------------------------------------------------ */
 import * as path from 'path';
 import {
-	workspace as Workspace, window as Window, ExtensionContext, TextDocument, OutputChannel, WorkspaceFolder, Uri
+    workspace as Workspace, window as Window, ExtensionContext, TextDocument, OutputChannel, WorkspaceFolder, Uri
 } from 'vscode';
-import { 
-	LanguageClient, LanguageClientOptions, TransportKind, ExecutableOptions, ServerOptions
+import {
+    LanguageClient, LanguageClientOptions, TransportKind, ExecutableOptions, ServerOptions
 } from 'vscode-languageclient/node';
 import { downloadThriftLanguageServer } from "./download"
 import { spawn } from 'child_process';
+import * as url from 'url';
 
 
 import { ErrorAction, CloseAction, ErrorHandler, Message } from 'vscode-languageclient/node'; // 请替换成实际的模块路径
@@ -20,35 +21,35 @@ const clients: Map<string, LanguageClient> = new Map();
 
 let _sortedWorkspaceFolders: string[] | undefined;
 function sortedWorkspaceFolders(): string[] {
-	if (_sortedWorkspaceFolders === void 0) {
-		_sortedWorkspaceFolders = Workspace.workspaceFolders ? Workspace.workspaceFolders.map(folder => {
-			let result = folder.uri.toString();
-			if (result.charAt(result.length - 1) !== '/') {
-				result = result + '/';
-			}
-			return result;
-		}).sort(
-			(a, b) => {
-				return a.length - b.length;
-			}
-		) : [];
-	}
-	return _sortedWorkspaceFolders;
+    if (_sortedWorkspaceFolders === void 0) {
+        _sortedWorkspaceFolders = Workspace.workspaceFolders ? Workspace.workspaceFolders.map(folder => {
+            let result = folder.uri.toString();
+            if (result.charAt(result.length - 1) !== '/') {
+                result = result + '/';
+            }
+            return result;
+        }).sort(
+            (a, b) => {
+                return a.length - b.length;
+            }
+        ) : [];
+    }
+    return _sortedWorkspaceFolders;
 }
 Workspace.onDidChangeWorkspaceFolders(() => _sortedWorkspaceFolders = undefined);
 
 function getOuterMostWorkspaceFolder(folder: WorkspaceFolder): WorkspaceFolder {
-	const sorted = sortedWorkspaceFolders();
-	for (const element of sorted) {
-		let uri = folder.uri.toString();
-		if (uri.charAt(uri.length - 1) !== '/') {
-			uri = uri + '/';
-		}
-		if (uri.startsWith(element)) {
-			return Workspace.getWorkspaceFolder(Uri.parse(element))!;
-		}
-	}
-	return folder;
+    const sorted = sortedWorkspaceFolders();
+    for (const element of sorted) {
+        let uri = folder.uri.toString();
+        if (uri.charAt(uri.length - 1) !== '/') {
+            uri = uri + '/';
+        }
+        if (uri.startsWith(element)) {
+            return Workspace.getWorkspaceFolder(Uri.parse(element))!;
+        }
+    }
+    return folder;
 }
 
 
@@ -75,33 +76,33 @@ export async function activate(context: ExtensionContext) {
 
     console.log("setup server options")
 
-	function didOpenTextDocument(document: TextDocument): void {
+    function didOpenTextDocument(document: TextDocument): void {
         console.log("didOpenTextDocument", document)
-		// We are only interested in language mode text
-		if (document.languageId !== 'thrift' || document.uri.scheme === 'untitled' || (document.uri.scheme !== 'file' )) {
-			return;
-		}
+        // We are only interested in language mode text
+        if (document.languageId !== 'thrift' || document.uri.scheme === 'untitled' || (document.uri.scheme !== 'file')) {
+            return;
+        }
 
-		const uri = document.uri;
-		let folder = Workspace.getWorkspaceFolder(uri);
-		// Files outside a folder can't be handled. This might depend on the language.
-		// Single file languages like JSON might handle files outside the workspace folders.
-		if (!folder) {
-			return;
-		}
-		// If we have nested workspace folders we only start a server on the outer most workspace folder.
-		folder = getOuterMostWorkspaceFolder(folder);
+        const uri = document.uri;
+        let folder = Workspace.getWorkspaceFolder(uri);
+        // Files outside a folder can't be handled. This might depend on the language.
+        // Single file languages like JSON might handle files outside the workspace folders.
+        if (!folder) {
+            return;
+        }
+        // If we have nested workspace folders we only start a server on the outer most workspace folder.
+        folder = getOuterMostWorkspaceFolder(folder);
 
-		if (!clients.has(folder.uri.toString())) {
+        if (!clients.has(folder.uri.toString())) {
             const exeOptions: ExecutableOptions = {
                 cwd: folder ? undefined : path.dirname(uri.fsPath),
             };
             let args: string[] = [];
 
-			const serverOptions: ServerOptions = {
-				run: { command: binaryPath, transport: TransportKind.stdio, args, options: exeOptions },
-				debug: { command: binaryPath, transport: TransportKind.stdio, args, options: exeOptions }
-			};
+            const serverOptions: ServerOptions = {
+                run: { command: binaryPath, transport: TransportKind.stdio, args, options: exeOptions },
+                debug: { command: binaryPath, transport: TransportKind.stdio, args, options: exeOptions }
+            };
 
             // let serverOptions = (): Thenable<any> => {
             //     // Start the server using child_process.spawn(...);
@@ -110,52 +111,58 @@ export async function activate(context: ExtensionContext) {
             //     childProcess.stderr.on('data', (data) => {
             //         console.error(`stderr: ${data}`);
             //       });
-                  
+
             //     childProcess.on('close', (code) => {
             //         console.log(`child process exited with code ${code}`);
             //       }); 
-                
+
             //     return childProcess; // Uses stdin/stdout for communication
             // }
 
-            const langName = "thriftls" 
+            const langName = "thriftls"
             const outputChannel: OutputChannel = Window.createOutputChannel(langName);
-			const clientOptions: LanguageClientOptions = {
-				documentSelector: [
-					{ scheme: 'file', language: 'thrift', pattern: `${folder.uri.fsPath}/**/*` }
-				],
-				diagnosticCollectionName: 'thrift-language-server',
-				workspaceFolder: folder,
-				outputChannel: outputChannel,
+            const clientOptions: LanguageClientOptions = {
+                documentSelector: [
+                    { scheme: 'file', language: 'thrift', pattern: `${folder.uri.fsPath}/**/*` }
+                ],
+                uriConverters: {
+                    // VS Code by default %-encodes even the colon after the drive letter
+                    // NodeJS handles it much better
+                    code2Protocol: uri => url.format(url.parse(uri.toString(true))),
+                    protocol2Code: str => Uri.parse(str)
+                },
+                diagnosticCollectionName: 'thrift-language-server',
+                workspaceFolder: folder,
+                outputChannel: outputChannel,
                 errorHandler: new MyErrorHandler,
-			};
-			const client = new LanguageClient('thrift-language-server', 'Thrift LSP Multi Server', serverOptions, clientOptions, true);
-			client.start();
-			clients.set(folder.uri.toString(), client);
+            };
+            const client = new LanguageClient('thrift-language-server', 'Thrift LSP Multi Server', serverOptions, clientOptions, true);
+            client.start();
+            clients.set(folder.uri.toString(), client);
             console.log("client start")
-		}
-	}
+        }
+    }
 
-	Workspace.onDidOpenTextDocument(didOpenTextDocument);
-	Workspace.textDocuments.forEach(didOpenTextDocument);
-	Workspace.onDidChangeWorkspaceFolders((event) => {
-		for (const folder  of event.removed) {
-			const client = clients.get(folder.uri.toString());
-			if (client) {
-				clients.delete(folder.uri.toString());
-				client.stop();
-			}
-		}
-	});
+    Workspace.onDidOpenTextDocument(didOpenTextDocument);
+    Workspace.textDocuments.forEach(didOpenTextDocument);
+    Workspace.onDidChangeWorkspaceFolders((event) => {
+        for (const folder of event.removed) {
+            const client = clients.get(folder.uri.toString());
+            if (client) {
+                clients.delete(folder.uri.toString());
+                client.stop();
+            }
+        }
+    });
 }
 
 export function deactivate(): Thenable<void> {
-	const promises: Thenable<void>[] = [];
-	if (defaultClient) {
-		promises.push(defaultClient.stop());
-	}
-	for (const client of clients.values()) {
-		promises.push(client.stop());
-	}
-	return Promise.all(promises).then(() => undefined);
+    const promises: Thenable<void>[] = [];
+    if (defaultClient) {
+        promises.push(defaultClient.stop());
+    }
+    for (const client of clients.values()) {
+        promises.push(client.stop());
+    }
+    return Promise.all(promises).then(() => undefined);
 }
